6cb6f068c6a4bf0e0e3213b91546befb99c5c9e5
Suberu 'Seyi <suberuseyi@gmail.com>
2025-09-05 13:46:17 +0100

    Environment-scoped appsetting.json files used to hold mailTo address read whenever a delete operation is carried out

--- CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
+++ CityInfo.API/obj/Debug/net8.0/CityInfo.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee226c5fca011f4a880813a3bac0cbc254fe20c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1759668defb07ec45b32bf7bba82f8b3a29eb244")]
 [assembly: System.Reflection.AssemblyProductAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CityInfo.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
